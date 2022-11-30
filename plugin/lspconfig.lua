f5bb20468a643aebdcba0774ba53e92eb4f1539c
BartekWinter <bw@eulertech.co>
2022-11-30 11:37:02 +0000

    Updated neovim config from ubuntu

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -63,3 +63,98 @@ require('lspconfig').gopls.setup{
   },
 	on_attach = on_attach,
 }
+-- " Order imports on save, like goimports does:
+-- "
+function OrgImports(wait_ms)
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+    for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+        else
+          vim.lsp.buf.execute_command(r.command)
+        end
+      end
+    end
+end
+
+vim.cmd([[
+" Configure Golang Environment.
+"
+fun! GoFumpt()
+  :silent !gofumpt -w %
+  :edit
+endfun
+autocmd FileType go map <buffer> <leader>p :call append(".", "fmt.Printf(\"%+v\\n\", )")<CR> <bar> :norm $a<CR><esc>j==$i
+autocmd FileType go map <buffer> <leader>e :call append(".", "if err != nil {return err}")<CR> <bar> :w<CR>
+autocmd BufWritePost *.go call GoFumpt()
+autocmd BufWritePost *.go :cex system('revive '..expand('%:p')) | cwindow
+autocmd BufWritePre *.go lua OrgImports(1000)
+" Configure LSP code navigation shortcuts
+" as found in :help lsp
+"
+nnoremap <silent> <c-]>     <cmd>lua vim.lsp.buf.definition()<CR>
+nnoremap <silent> <c-k>     <cmd>lua vim.lsp.buf.signature_help()<CR>
+nnoremap <silent> K         <cmd>lua vim.lsp.buf.hover()<CR>
+nnoremap <silent> gi        <cmd>lua vim.lsp.buf.implementation()<CR>
+nnoremap <silent> gc        <cmd>lua vim.lsp.buf.incoming_calls()<CR>
+nnoremap <silent> gd        <cmd>lua vim.lsp.buf.type_definition()<CR>
+nnoremap <silent> gr        <cmd>lua vim.lsp.buf.references()<CR>
+nnoremap <silent> gn        <cmd>lua vim.lsp.buf.rename()<CR>
+nnoremap <silent> gs        <cmd>lua vim.lsp.buf.document_symbol()<CR>
+nnoremap <silent> gw        <cmd>lua vim.lsp.buf.workspace_symbol()<CR>
+
+" Replaced LSP implementation with code action plugin...
+"
+" nnoremap <silent> ga        <cmd>lua vim.lsp.buf.code_action()<CR>
+"
+nnoremap <silent> ga        <cmd>CodeActionMenu<CR>
+
+nnoremap <silent> [x        <cmd>lua vim.diagnostic.goto_prev()<CR>
+nnoremap <silent> ]x        <cmd>lua vim.diagnostic.goto_next()<CR>
+nnoremap <silent> ]s        <cmd>lua vim.diagnostic.show()<CR>
+
+" Replaced LSP implementation with trouble plugin...
+"
+" nnoremap <silent> <space>q  <cmd>lua vim.diagnostic.setloclist()<CR>
+"
+nnoremap <silent> <space>q  <cmd>Trouble<CR>
+]])
+
+-- " Setup Completion
+-- " https://github.com/hrsh7th/nvim-cmp#recommended-configuration
+-- "
+
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+    ['<Tab>'] = cmp.mapping.select_next_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping.close(),
+    ['<CR>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    })
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' },
+    { name = 'path' },
+    { name = 'buffer' },
+    { name = 'nvim_lsp_signature_help' },
+  },
+})
+
+
