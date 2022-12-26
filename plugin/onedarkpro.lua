73fd65661655cdba293d946bd39cea1751690137
bwintertkb <66641948+bwintertkb@users.noreply.github.com>
2022-12-26 23:57:47 +0000

    Update onedarkpro.lua

--- plugin/onedarkpro.lua
+++ plugin/onedarkpro.lua
@@ -30,7 +30,7 @@
     --transparency = false, -- Use a transparent background?
     --terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
     --highlight_inactive_windows = false, -- When the window is out of focus, change the normal background?
-  }
-})
+--  }
+--})
 
 -- vim.cmd("colorscheme onedark")
