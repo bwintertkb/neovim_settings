3210473344b9c46464e1722cef0b6c3ee34f9c92
bwintertkb <66641948+bwintertkb@users.noreply.github.com>
2023-03-15 23:23:16 +0000

    Update toggleterm.lua

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -1,2 +1,5 @@
-vim.o.shell = "/bin/bash"
+
+if vim.fn.has('win32') then
+   vim.o.shell = "/bin/bash"
+end
 require("toggleterm").setup({shell = vim.o.shell, direction = "float", open_mapping = [[<C-j>]], close_mapping = [[<C-j>]], size = 10 })
