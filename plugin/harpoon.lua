1e890681c90b456a4c0559705b9abf18a7159128
bwintertkb <66641948+bwintertkb@users.noreply.github.com>
2023-05-29 14:17:42 +0100

    Create harpoon.lua

---
+++ plugin/harpoon.lua
@@ -0,0 +1,5 @@
+require('harpoon').setup()
+vim.keymap.set('n', '<A-a>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', {noremap = true})
+vim.keymap.set('n', '<A-s>', ':lua require("harpoon.mark").add_file()<CR>', {noremap = true})
+vim.keymap.set('n', '<A-d>', ':lua require("harpoon.ui").nav_prev()<CR>', {noremap = true})
+vim.keymap.set('n', '<A-f>', ':lua require("harpoon.ui").nav_next()<CR>', {noremap = true})
