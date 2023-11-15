fdbaa1c1fc5602e07ca7dfbe50a4040208ed803c
Bartek Winter <bw@eulertech.co>
2023-11-15 13:45:10 +0000

    Updated neovim config

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -148,7 +148,12 @@ require("lspconfig")["yamlls"].setup({
 })
 
 -- HTML
-require("lspconfig").html.setup{}
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+require'lspconfig'.html.setup {
+  capabilities = capabilities,
+}
 -- HTMX
 require("lspconfig").htmx.setup{}
