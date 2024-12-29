3a540024000693c35ce8a9ee79027a9345129978
Bartek Winter <66641948+bwintertkb@users.noreply.github.com>
2024-12-29 17:12:08 +0000

    Update copilot.lua

--- plugin/copilot.lua
+++ plugin/copilot.lua
@@ -1,46 +1,46 @@
 require('copilot').setup({
-  panel = {
-    enabled = false,
-    auto_refresh = false,
-    keymap = {
-      jump_prev = "[[",
-      jump_next = "]]",
-      accept = "<CR>",
-      refresh = "gr",
-      open = "<M-CR>"
-    },
-    layout = {
-      position = "bottom", -- | top | left | right
-      ratio = 0.4
-    },
-  },
-  suggestion = {
-    enabled = false,
-    auto_trigger = false,
-    debounce = 75,
-    keymap = {
-      accept = "<C-f>",
-      accept_word = false,
-      accept_line = false,
-      next = "<M-\\>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-  },
-  filetypes = {
-    yaml = false,
-    markdown = false,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
-  },
-  copilot_node_command = 'node', -- Node.js version must be > 16.x
-  server_opts_overrides = {},
+	panel = {
+		enabled = false,
+		auto_refresh = false,
+		keymap = {
+			jump_prev = "[[",
+			jump_next = "]]",
+			accept = "<C-x><C-z>", -- Use an obscure keybinding
+			refresh = "gr",
+			open = "<M-CR>"
+		},
+		layout = {
+			position = "bottom", -- | top | left | right
+			ratio = 0.4
+		},
+	},
+	suggestion = {
+		enabled = false,
+		auto_trigger = false,
+		debounce = 75,
+		keymap = {
+			accept = "<C-f>",
+			accept_word = false,
+			accept_line = false,
+			next = "<M-\\>",
+			prev = "<M-[>",
+			dismiss = "<C-]>",
+		},
+	},
+	filetypes = {
+		yaml = false,
+		markdown = false,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
+	copilot_node_command = 'node', -- Node.js version must be > 16.x
+	server_opts_overrides = {},
 })
 
-vim.api.nvim_set_keymap('n', '<leader>g', ':Copilot enable<CR>', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<leader>G', ':Copilot disable<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>g', ':Copilot enable<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>G', ':Copilot disable<CR>', { noremap = true, silent = true })
