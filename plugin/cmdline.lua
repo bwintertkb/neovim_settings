a50e3010aac036e73daa832b4270bfab966408fe
Bartek Winter <66641948+bwintertkb@users.noreply.github.com>
2025-04-13 22:26:32 +0100

    Create cmdline.lua

---
+++ plugin/cmdline.lua
@@ -0,0 +1,23 @@
+-- `/` cmdline setup.
+local cmp = require("cmp")
+cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	}, {
+		{
+			name = 'cmdline',
+			option = {
+				ignore_cmds = { 'Man', '!' }
+			}
+		}
+	})
+})
