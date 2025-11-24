c608b032bc59dac8b21beeb9a654a19e7b791770
bwintertkb <bartekwinter3@gmail.com>
2025-11-24 22:26:58 +0000

    Updated neovim config

--- plugin/fzf.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<c-u>",
-  "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
-
-vim.keymap.set("n", "<c-p>",
-  "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
